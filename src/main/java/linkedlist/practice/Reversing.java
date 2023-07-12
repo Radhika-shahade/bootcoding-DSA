2fb94e614895d4d2c7da9951cdec0858be2e8b03
Radhika shahade <radhikashahade0804@gmail.com>
2023-07-12 11:05:07 +0530

    LINKEDLIST practice

--- src/main/java/linkedlist/practice/Reversing.java
+++ src/main/java/linkedlist/practice/Reversing.java
@@ -1,46 +1,55 @@
-//package linkedlist.practice;
+package linkedlist.practice;
 //
-//public class Reversing {
-//    public static Node reversing(Node head)
-//    {
-//        Node temp=head;
-//        Node previous=head;
-//        temp=temp.next;
-//        previous=temp;
-//
-//        temp.;
-//        return head;
-//    }
-//    public static Node createLinkedList()
-//    {
-//        Node firstNode=new Node(1);
-//        Node secondNode=new Node(2);
-//        Node thirdNode=new Node(3);
-//        Node fourthNode=new Node(4);
-//
-//        Node head=firstNode;
-//        firstNode.next=secondNode;
-//        secondNode.next=thirdNode;
-//        thirdNode.next=fourthNode;
-//        fourthNode.next=null;
-//
-//        return head;
-//    }
-//    public static void printLinkedList(Node head)
-//    {
-//        Node temp=head;
-//        while(temp!=null)
-//        {
-//            System.out.println(temp.data);
-//            temp=temp.next;
-//        }
-//
-//    }
-//    public static void main(String[] args) {
-//        Node head= createLinkedList();
-//       head= reversing(head);
-//       printLinkedList(head);
-//
-//    }
-//
-//}
+public class Reversing {
+    public static void main(String[] args) {
+       Node head= createList();
+       Node s =reverse(head);
+       print(s);
+    }
+    public static Node reverse(Node head)
+    {
+        Node previous=null;
+        Node current=head;
+
+        while(current!=null)
+        {
+            Node temp=current.next;
+            current.next=previous;
+            previous= current;
+            current=temp;
+
+
+        }
+        return previous;
+    }
+    public static Node createList()
+    {
+        Node firstNode =new Node(10);
+        Node secondNode = new Node(20);
+        Node thirdNode=new Node(30);
+        Node fourthNode= new Node(40);
+        Node fifthNode= new Node(70);
+
+        Node head=firstNode;
+        firstNode.next=secondNode;
+        secondNode.next=thirdNode;
+        thirdNode.next=fourthNode;
+        fourthNode.next=fifthNode;
+        fifthNode.next=null;
+        return head;
+    }
+    public static void print(Node head)
+    {
+        Node temp=head;
+        while(temp!=null)
+        {
+            System.out.println(temp.data);
+            temp=temp.next;
+        }
+    }
+
+
+
+
+
+}
