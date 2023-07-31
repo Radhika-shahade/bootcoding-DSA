5544e05a20dfe439350ec7089b75dffcae519d57
Radhika shahade <radhikashahade0804@gmail.com>
2023-07-31 23:57:13 +0530

    tree solution

---
+++ src/main/java/tree/binarysearchtree/leetcode/TreeNode.java
@@ -0,0 +1,18 @@
+package tree.binarysearchtree.leetcode;
+
+public class TreeNode  {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(int data)
+    {
+        this.val=val;
+    }
+
+    public TreeNode(int data, TreeNode left,TreeNode right)
+    {
+        this.val=val;
+        this.left=left;
+        this.right=right;
+    }
+}
