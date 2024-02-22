778892f5ad9d439d91686734350f54b54e6689d5
Radhika shahade <radhikashahade0804@gmail.com>
2024-02-22 22:57:52 +0530

    stack using queue

---
+++ src/main/java/queue/leetcode/ImplementStackUsingQueues.java
@@ -0,0 +1,8 @@
+package queue.leetcode;
+
+public class ImplementStackUsingQueues {
+    public static void main(String[] args) {
+        String s=" gfdgsahdg";
+//        s.substring(0,);
+    }
+}
